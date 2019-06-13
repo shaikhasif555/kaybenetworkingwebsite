7d9532e6bb7dbdbfc652048ffa4071833fd00740
shaikhasif555 <43498656+shaikhasif555@users.noreply.github.com>
2019-06-13 19:59:53 +0400

    lib file
    lib file

---
+++ js/main123.js
@@ -0,0 +1,23 @@
+﻿(function ($) {
+    "use strict";
+
+    /* ********************************************
+		1. jQuery MeanMenu
+	******************************************** */
+    jQuery('nav#dropdown').meanmenu();
+
+})(jQuery);
+
+/* ********************************************
+    2. STICKY sticky-header
+******************************************** */
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 1) {
+        $('#sticky-header').addClass("sticky");
+    }
+    else {
+        $('#sticky-header').removeClass("sticky");
+    }
+});
+/* ********************************************************* */
+
---
+++ js/menu-plugins.js
@@ -0,0 +1,300 @@
+﻿
+
+
+/*-------------------------------------------------------------
+    6. meanMenu jquery menu plugin / mobile menu
+---------------------------------------------------------------*/
+
+/*!
+* jQuery meanMenu v2.0.8
+* @Copyright (C) 2012-2014 Chris Wharton @ MeanThemes (https://github.com/meanthemes/meanMenu)
+*
+*/
+/*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* THIS SOFTWARE AND DOCUMENTATION IS PROVIDED "AS IS," AND COPYRIGHT
+* HOLDERS MAKE NO REPRESENTATIONS OR WARRANTIES, EXPRESS OR IMPLIED,
+* INCLUDING BUT NOT LIMITED TO, WARRANTIES OF MERCHANTABILITY OR
+* FITNESS FOR ANY PARTICULAR PURPOSE OR THAT THE USE OF THE SOFTWARE
+* OR DOCUMENTATION WILL NOT INFRINGE ANY THIRD PARTY PATENTS,
+* COPYRIGHTS, TRADEMARKS OR OTHER RIGHTS.COPYRIGHT HOLDERS WILL NOT
+* BE LIABLE FOR ANY DIRECT, INDIRECT, SPECIAL OR CONSEQUENTIAL
+* DAMAGES ARISING OUT OF ANY USE OF THE SOFTWARE OR DOCUMENTATION.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://gnu.org/licenses/>.
+*
+* Find more information at http://www.meanthemes.com/plugins/meanmenu/
+*
+*/
+(function ($) {
+    "use strict";
+    $.fn.meanmenu = function (options) {
+        var defaults = {
+            meanMenuTarget: jQuery(this), // Target the current HTML markup you wish to replace
+            meanMenuContainer: '.mobile-menu-area .container', // Choose where meanmenu will be placed within the HTML
+            meanMenuClose: "X", // single character you want to represent the close menu button
+            meanMenuCloseSize: "18px", // set font size of close button
+            meanMenuOpen: "<span /><span /><span />", // text/markup you want when menu is closed
+            meanRevealPosition: "right", // left right or center positions
+            meanRevealPositionDistance: "0", // Tweak the position of the menu
+            meanRevealColour: "", // override CSS colours for the reveal background
+            meanScreenWidth: "991", // set the screen width you want meanmenu to kick in at
+            meanNavPush: "", // set a height here in px, em or % if you want to budge your layout now the navigation is missing.
+            meanShowChildren: true, // true to show children in the menu, false to hide them
+            meanExpandableChildren: true, // true to allow expand/collapse children
+            meanExpand: "+", // single character you want to represent the expand for ULs
+            meanContract: "-", // single character you want to represent the contract for ULs
+            meanRemoveAttrs: false, // true to remove classes and IDs, false to keep them
+            onePage: false, // set to true for one page sites
+            meanDisplay: "block", // override display method for table cell based layouts e.g. table-cell
+            removeElements: "" // set to hide page elements
+        };
+        options = $.extend(defaults, options);
+
+        // get browser width
+        var currentWidth = window.innerWidth || document.documentElement.clientWidth;
+
+        return this.each(function () {
+            var meanMenu = options.meanMenuTarget;
+            var meanContainer = options.meanMenuContainer;
+            var meanMenuClose = options.meanMenuClose;
+            var meanMenuCloseSize = options.meanMenuCloseSize;
+            var meanMenuOpen = options.meanMenuOpen;
+            var meanRevealPosition = options.meanRevealPosition;
+            var meanRevealPositionDistance = options.meanRevealPositionDistance;
+            var meanRevealColour = options.meanRevealColour;
+            var meanScreenWidth = options.meanScreenWidth;
+            var meanNavPush = options.meanNavPush;
+            var meanRevealClass = ".meanmenu-reveal";
+            var meanShowChildren = options.meanShowChildren;
+            var meanExpandableChildren = options.meanExpandableChildren;
+            var meanExpand = options.meanExpand;
+            var meanContract = options.meanContract;
+            var meanRemoveAttrs = options.meanRemoveAttrs;
+            var onePage = options.onePage;
+            var meanDisplay = options.meanDisplay;
+            var removeElements = options.removeElements;
+
+            //detect known mobile/tablet usage
+            var isMobile = false;
+            if ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || (navigator.userAgent.match(/iPad/i)) || (navigator.userAgent.match(/Android/i)) || (navigator.userAgent.match(/Blackberry/i)) || (navigator.userAgent.match(/Windows Phone/i))) {
+                isMobile = true;
+            }
+
+            if ((navigator.userAgent.match(/MSIE 8/i)) || (navigator.userAgent.match(/MSIE 7/i))) {
+                // add scrollbar for IE7 & 8 to stop breaking resize function on small content sites
+                jQuery('html').css("overflow-y", "scroll");
+            }
+
+            var meanRevealPos = "";
+            var meanCentered = function () {
+                if (meanRevealPosition === "center") {
+                    var newWidth = window.innerWidth || document.documentElement.clientWidth;
+                    var meanCenter = ((newWidth / 2) - 22) + "px";
+                    meanRevealPos = "left:" + meanCenter + ";right:auto;";
+
+                    if (!isMobile) {
+                        jQuery('.meanmenu-reveal').css("left", meanCenter);
+                    } else {
+                        jQuery('.meanmenu-reveal').animate({
+                            left: meanCenter
+                        });
+                    }
+                }
+            };
+
+            var menuOn = false;
+            var meanMenuExist = false;
+
+
+            if (meanRevealPosition === "right") {
+                meanRevealPos = "right:" + meanRevealPositionDistance + ";left:auto;";
+            }
+            if (meanRevealPosition === "left") {
+                meanRevealPos = "left:" + meanRevealPositionDistance + ";right:auto;";
+            }
+            // run center function
+            meanCentered();
+
+            // set all styles for mean-reveal
+            var $navreveal = "";
+
+            var meanInner = function () {
+                // get last class name
+                if (jQuery($navreveal).is(".meanmenu-reveal.meanclose")) {
+                    $navreveal.html(meanMenuClose);
+                } else {
+                    $navreveal.html(meanMenuOpen);
+                }
+            };
+
+            // re-instate original nav (and call this on window.width functions)
+            var meanOriginal = function () {
+                jQuery('.mean-bar,.mean-push').remove();
+                jQuery(meanContainer).removeClass("mean-container");
+                jQuery(meanMenu).css('display', meanDisplay);
+                menuOn = false;
+                meanMenuExist = false;
+                jQuery(removeElements).removeClass('mean-remove');
+            };
+
+            // navigation reveal
+            var showMeanMenu = function () {
+                var meanStyles = "background:" + meanRevealColour + ";color:" + meanRevealColour + ";" + meanRevealPos;
+                if (currentWidth <= meanScreenWidth) {
+                    jQuery(removeElements).addClass('mean-remove');
+                    meanMenuExist = true;
+                    // add class to body so we don't need to worry about media queries here, all CSS is wrapped in '.mean-container'
+                    jQuery(meanContainer).addClass("mean-container");
+                    jQuery('.mean-container').prepend('<div class="mean-bar"><a href="#nav" class="meanmenu-reveal" style="' + meanStyles + '">Show Navigation</a><nav class="mean-nav"></nav></div>');
+
+                    //push meanMenu navigation into .mean-nav
+                    var meanMenuContents = jQuery(meanMenu).html();
+                    jQuery('.mean-nav').html(meanMenuContents);
+
+                    // remove all classes from EVERYTHING inside meanmenu nav
+                    if (meanRemoveAttrs) {
+                        jQuery('nav.mean-nav ul, nav.mean-nav ul *').each(function () {
+                            // First check if this has mean-remove class
+                            if (jQuery(this).is('.mean-remove')) {
+                                jQuery(this).attr('class', 'mean-remove');
+                            } else {
+                                jQuery(this).removeAttr("class");
+                            }
+                            jQuery(this).removeAttr("id");
+                        });
+                    }
+
+                    // push in a holder div (this can be used if removal of nav is causing layout issues)
+                    jQuery(meanMenu).before('<div class="mean-push" />');
+                    jQuery('.mean-push').css("margin-top", meanNavPush);
+
+                    // hide current navigation and reveal mean nav link
+                    jQuery(meanMenu).hide();
+                    jQuery(".meanmenu-reveal").show();
+
+                    // turn 'X' on or off
+                    jQuery(meanRevealClass).html(meanMenuOpen);
+                    $navreveal = jQuery(meanRevealClass);
+
+                    //hide mean-nav ul
+                    jQuery('.mean-nav ul').hide();
+
+                    // hide sub nav
+                    if (meanShowChildren) {
+                        // allow expandable sub nav(s)
+                        if (meanExpandableChildren) {
+                            jQuery('.mean-nav ul ul').each(function () {
+                                if (jQuery(this).children().length) {
+                                    jQuery(this, 'li:first').parent().append('<a class="mean-expand" href="#" style="font-size: ' + meanMenuCloseSize + '">' + meanExpand + '</a>');
+                                }
+                            });
+                            jQuery('.mean-expand').on("click", function (e) {
+                                e.preventDefault();
+                                if (jQuery(this).hasClass("mean-clicked")) {
+                                    jQuery(this).text(meanExpand);
+                                    jQuery(this).prev('ul').slideUp(300, function () { });
+                                } else {
+                                    jQuery(this).text(meanContract);
+                                    jQuery(this).prev('ul').slideDown(300, function () { });
+                                }
+                                jQuery(this).toggleClass("mean-clicked");
+                            });
+                        } else {
+                            jQuery('.mean-nav ul ul').show();
+                        }
+                    } else {
+                        jQuery('.mean-nav ul ul').hide();
+                    }
+
+                    // add last class to tidy up borders
+                    jQuery('.mean-nav ul li').last().addClass('mean-last');
+                    $navreveal.removeClass("meanclose");
+                    jQuery($navreveal).click(function (e) {
+                        e.preventDefault();
+                        if (menuOn === false) {
+                            $navreveal.css("text-align", "center");
+                            $navreveal.css("text-indent", "0");
+                            $navreveal.css("font-size", meanMenuCloseSize);
+                            jQuery('.mean-nav ul:first').slideDown();
+                            menuOn = true;
+                        } else {
+                            jQuery('.mean-nav ul:first').slideUp();
+                            menuOn = false;
+                        }
+                        $navreveal.toggleClass("meanclose");
+                        meanInner();
+                        jQuery(removeElements).addClass('mean-remove');
+                    });
+
+                    // for one page websites, reset all variables...
+                    if (onePage) {
+                        jQuery('.mean-nav ul > li > a:first-child').on("click", function () {
+                            jQuery('.mean-nav ul:first').slideUp();
+                            menuOn = false;
+                            jQuery($navreveal).toggleClass("meanclose").html(meanMenuOpen);
+                        });
+                    }
+                } else {
+                    meanOriginal();
+                }
+            };
+
+            if (!isMobile) {
+                // reset menu on resize above meanScreenWidth
+                jQuery(window).resize(function () {
+                    currentWidth = window.innerWidth || document.documentElement.clientWidth;
+                    if (currentWidth > meanScreenWidth) {
+                        meanOriginal();
+                    } else {
+                        meanOriginal();
+                    }
+                    if (currentWidth <= meanScreenWidth) {
+                        showMeanMenu();
+                        meanCentered();
+                    } else {
+                        meanOriginal();
+                    }
+                });
+            }
+
+            jQuery(window).resize(function () {
+                // get browser width
+                currentWidth = window.innerWidth || document.documentElement.clientWidth;
+
+                if (!isMobile) {
+                    meanOriginal();
+                    if (currentWidth <= meanScreenWidth) {
+                        showMeanMenu();
+                        meanCentered();
+                    }
+                } else {
+                    meanCentered();
+                    if (currentWidth <= meanScreenWidth) {
+                        if (meanMenuExist === false) {
+                            showMeanMenu();
+                        }
+                    } else {
+                        meanOriginal();
+                    }
+                }
+            });
+
+            // run main menuMenu function on load
+            showMeanMenu();
+        });
+    };
+
+
+    /*-------------------------------------------------------------
+        7. countdown js jquery 
+    ---------------------------------------------------------------*/
+    !function (t) { "use strict"; "function" == typeof define && define.amd ? define(["jquery"], t) : t(jQuery) }(function (t) { "use strict"; function e(t) { if (t instanceof Date) return t; if (String(t).match(o)) return String(t).match(/^[0-9]*$/) && (t = Number(t)), String(t).match(/\-/) && (t = String(t).replace(/\-/g, "/")), new Date(t); throw new Error("Couldn't cast `" + t + "` to a date object.") } function s(t) { var e = t.toString().replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1"); return new RegExp(e) } function n(t) { return function (e) { var n = e.match(/%(-|!)?[A-Z]{1}(:[^;]+;)?/gi); if (n) for (var a = 0, o = n.length; o > a; ++a) { var r = n[a].match(/%(-|!)?([a-zA-Z]{1})(:[^;]+;)?/), l = s(r[0]), c = r[1] || "", u = r[3] || "", f = null; r = r[2], h.hasOwnProperty(r) && (f = h[r], f = Number(t[f])), null !== f && ("!" === c && (f = i(u, f)), "" === c && 10 > f && (f = "0" + f.toString()), e = e.replace(l, f.toString())) } return e = e.replace(/%%/, "%") } } function i(t, e) { var s = "s", n = ""; return t && (t = t.replace(/(:|;|\s)/gi, "").split(/\,/), 1 === t.length ? s = t[0] : (n = t[0], s = t[1])), 1 === Math.abs(e) ? n : s } var a = [], o = [], r = { precision: 100, elapse: !1 }; o.push(/^[0-9]*$/.source), o.push(/([0-9]{1,2}\/){2}[0-9]{4}( [0-9]{1,2}(:[0-9]{2}){2})?/.source), o.push(/[0-9]{4}([\/\-][0-9]{1,2}){2}( [0-9]{1,2}(:[0-9]{2}){2})?/.source), o = new RegExp(o.join("|")); var h = { Y: "years", m: "months", n: "daysToMonth", w: "weeks", d: "daysToWeek", D: "totalDays", H: "hours", M: "minutes", S: "seconds" }, l = function (e, s, n) { this.el = e, this.$el = t(e), this.interval = null, this.offset = {}, this.options = t.extend({}, r), this.instanceNumber = a.length, a.push(this), this.$el.data("countdown-instance", this.instanceNumber), n && ("function" == typeof n ? (this.$el.on("update.countdown", n), this.$el.on("stoped.countdown", n), this.$el.on("finish.countdown", n)) : this.options = t.extend({}, r, n)), this.setFinalDate(s), this.start() }; t.extend(l.prototype, { start: function () { null !== this.interval && clearInterval(this.interval); var t = this; this.update(), this.interval = setInterval(function () { t.update.call(t) }, this.options.precision) }, stop: function () { clearInterval(this.interval), this.interval = null, this.dispatchEvent("stoped") }, toggle: function () { this.interval ? this.stop() : this.start() }, pause: function () { this.stop() }, resume: function () { this.start() }, remove: function () { this.stop.call(this), a[this.instanceNumber] = null, delete this.$el.data().countdownInstance }, setFinalDate: function (t) { this.finalDate = e(t) }, update: function () { if (0 === this.$el.closest("html").length) return void this.remove(); var e, s = void 0 !== t._data(this.el, "events"), n = new Date; e = this.finalDate.getTime() - n.getTime(), e = Math.ceil(e / 1e3), e = !this.options.elapse && 0 > e ? 0 : Math.abs(e), this.totalSecsLeft !== e && s && (this.totalSecsLeft = e, this.elapsed = n >= this.finalDate, this.offset = { seconds: this.totalSecsLeft % 60, minutes: Math.floor(this.totalSecsLeft / 60) % 60, hours: Math.floor(this.totalSecsLeft / 60 / 60) % 24, days: Math.floor(this.totalSecsLeft / 60 / 60 / 24) % 7, daysToWeek: Math.floor(this.totalSecsLeft / 60 / 60 / 24) % 7, daysToMonth: Math.floor(this.totalSecsLeft / 60 / 60 / 24 % 30.4368), totalDays: Math.floor(this.totalSecsLeft / 60 / 60 / 24), weeks: Math.floor(this.totalSecsLeft / 60 / 60 / 24 / 7), months: Math.floor(this.totalSecsLeft / 60 / 60 / 24 / 30.4368), years: Math.abs(this.finalDate.getFullYear() - n.getFullYear()) }, this.options.elapse || 0 !== this.totalSecsLeft ? this.dispatchEvent("update") : (this.stop(), this.dispatchEvent("finish"))) }, dispatchEvent: function (e) { var s = t.Event(e + ".countdown"); s.finalDate = this.finalDate, s.elapsed = this.elapsed, s.offset = t.extend({}, this.offset), s.strftime = n(this.offset), this.$el.trigger(s) } }), t.fn.countdown = function () { var e = Array.prototype.slice.call(arguments, 0); return this.each(function () { var s = t(this).data("countdown-instance"); if (void 0 !== s) { var n = a[s], i = e[0]; l.prototype.hasOwnProperty(i) ? n[i].apply(n, e.slice(1)) : null === String(i).match(/^[$A-Z_][0-9A-Z_$]*$/i) ? (n.setFinalDate.call(n, i), n.start()) : t.error("Method %s does not exist on jQuery.countdown".replace(/\%s/gi, i)) } else new l(this, e[0], e[1]) }) } });
+
+})(jQuery);
+
